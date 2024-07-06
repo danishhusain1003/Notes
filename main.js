d7509a635e10a853fb2fef38989dbcddad247645
DanishHussain <danishhussain1003@gmail.com>
2024-07-06 17:32:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+// FIrst Class Topic (Hello World Print)
+// console.log("Hello World")
+// Variable
+// let userName = "Hamza Alvi"
+// userName="Ameen Alam"
+// const classesCover = "THursday"
+// let age = 20
+// if (true){
+//     let age=22
+//     console.log(age,"if age")
+// }
+// console.log(age,"age")
+// Data Types
+// // Primitive Types (0.5 bytes to 64 bits)
+// 1) String    // let userName = "Danish Hussain"
+// 2) Number    // let age = 22
+// 3) Boolean   // let available = true/false
+// 4) Undefined // let noValue;            ,  noValue = undefined; (Khaali Daraz)
+// 5) Null      //                              (No Daraz, daraz hi nahi hai)
+// 6) 
+// 7) 
+// Variable k andar variable assign krdiaa (Daraz k andar daraz), this is 
+// called "Primitive Type"
+// let dob = 22;
+// let dob2 = dob + 1;
+// console.log(dob2, "dob2");
+// console.log(dob,"dob")
+// 1000000000000000000000000000000000000000000000000000000000000000000000000
+// 0000000000000000000000000000000000000000000000000000000000000000000000000
+// data type : big integer (used in banking sector)
+// Non-Primitive Type
+// Object
+// let obj = {}
+// // Array
+// let arr = []
+// // SimpleFunction
+// function helloworld(){
+// }
+// // Arrow/Lenda Function
+// const sayHello = ()=>{}
+// let obj = {
+//     name: "Hello World",
+//     class: "thursday",
+// }
+// obj.name='Sir Aziz'
+// // console.log(obj)
+// Error Types
+// 1) Syntax Error (Code typing or spelling mistake)
+// 2) Type Error   (type kuch btai or de kuch or rahe hein)
+// 3) Accessibility Error (bloack scope sa bhar kisi word ko use nahi kar 
+//                          sakta ha)
+// Strong Typing
+// kisi bhi variable ya function mein uski Type define krna "strong typing" 
+// kehlati hai.
+// Any Type
+// let userName:any="Hamza Alivi";
+// userName=56
+// Unknown Type
+var userName = 1.255555555;
+if (typeof userName === "string") {
+    console.log(userName.toUpperCase());
+}
+else if (typeof userName === "number") {
+    console.log(userName.toFixed(2));
+}
---
+++ main.ts
@@ -0,0 +1,103 @@
+// FIrst Class Topic (Hello World Print)
+// console.log("Hello World")
+// Variable
+// let userName = "Hamza Alvi"
+// userName="Ameen Alam"
+// const classesCover = "THursday"
+// let age = 20
+// if (true){
+//     let age=22
+//     console.log(age,"if age")
+// }
+// console.log(age,"age")
+
+// Data Types
+// // Primitive Types (0.5 bytes to 64 bits)
+// 1) String    // let userName = "Danish Hussain"
+// 2) Number    // let age = 22
+// 3) Boolean   // let available = true/false
+// 4) Undefined // let noValue;            ,  noValue = undefined; (Khaali Daraz)
+// 5) Null      //                              (No Daraz, daraz hi nahi hai)
+// 6) Never
+// 7) Unknown
+
+// Variable k andar variable assign krdiaa (Daraz k andar daraz), this is 
+// called "Primitive Type"
+// let dob = 22;
+// let dob2 = dob + 1;
+// console.log(dob2, "dob2");
+// console.log(dob,"dob")
+
+// 1000000000000000000000000000000000000000000000000000000000000000000000000
+// 0000000000000000000000000000000000000000000000000000000000000000000000000
+// data type : big integer (used in banking sector)
+
+// Non-Primitive Type
+// Object
+// let obj = {}
+// // Array
+// let arr = []
+// // SimpleFunction
+// function helloworld(){
+// }
+// // Arrow/Lenda Function
+// const sayHello = ()=>{}
+
+// let obj = {
+//     name: "Hello World",
+//     class: "thursday",
+// }
+// obj.name='Sir Aziz'
+// // console.log(obj)
+
+// Error Types
+// 1) Syntax Error (Code typing or spelling mistake)
+// 2) Type Error   (type kuch btai or de kuch or rahe hein)
+// 3) Accessibility Error (bloack scope sa bhar kisi word ko use nahi kar 
+//                          sakta ha)
+
+// Strong Typing
+// kisi bhi variable ya function mein uski Type define krna "strong typing" 
+// kehlati hai.
+
+// Any Type
+// let userName:any="Hamza Alivi";
+// userName=56
+
+// Unknown Type
+// let userName: unknown = 1.255555555
+// if(typeof userName === "string"){
+//     console.log(userName.toUpperCase())
+// }
+// else if (typeof userName === "number"){
+//     console.log(userName.toFixed(2))
+// }
+
+//  Spelling Cases
+//  Camel Case = userName
+//  Pascal Case = UserName
+//  Small Case = username
+//  Under score Case = _userName
+//  Dollar Case = $ userName
+
+//  Note
+//  Underscore and Dollar case are valid for variable declaration and
+//  all of the other cases are invalid.
+
+// A 'variable' can change or reassign but 'const' cannot change or reassign. 
+
+// {} curly brackets are block-scopped.
+
+// if else, functions and objects, include {} curly brackets.
+
+// if(true) lagayengay to condition lazmi chalegi.
+
+// if(false) lagane se condition nhi chalegi.
+
+// Terminal javascript mein Pehle {} curly bracket k code ko check karega 
+// phr main file k code ko.
+
+// index arrays ki value ko access krne k liye hota hai.
+
+// j.son object quotations yaani "" mein hota hai, this is the alternate
+// 'xml' file.
